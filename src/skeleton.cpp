fc2ab30b8f98cfd977cf9a413f1f365dd1d9c09e
Brooke <brookeydai@gmail.com>
2020-03-17 18:03:38 -0700

    Added function to recreate images given centers of max disks and their radii

--- src/skeleton.cpp
+++ src/skeleton.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <string>
 #include "PNG.h"
 
 using namespace std;
 
+#define BLACKPIXEL Pixel(0, 0, 0, 255)
+
+static bool isPixelValid(unsigned int x, unsigned int y, vector<vector<unsigned int>> & v)
+{
+    return (y >= 0 && y < v.size() && x >= 0 && x < v[0].size());
+}
+
 unsigned int getPixelDistance (unsigned int x,
                                unsigned int y,
                                vector<vector<unsigned int>> & distance_map)
 {
-    if (y < 0 || y >= distance_map.size() || x < 0 || x >= distance_map[0].size())
+    if (!isPixelValid(x, y, distance_map))
     {
         return 0;
     }
@@ -21,7 +29,7 @@ void setPixelDistance (unsigned int x,
                        unsigned int distVal,
                        vector<vector<unsigned int>> & distance_map)
 {
-    if (y < 0 || y >= distance_map.size() || x < 0 || x >= distance_map[0].size())
+    if (!isPixelValid(x, y, distance_map))
     {
         cout << __FUNCTION__ << ": ERROR could not set pixel distance at x=" << x << " y=" << y << endl;
     }
@@ -37,7 +45,7 @@ void getDistanceMap (vector<vector<unsigned int>> & distance_map, PNG & img)
     {
         for (int x = 0; x < img.getWidth(); x++)
         {
-            if (img.getPixel(x, y).approximate(Pixel(0,0,0,255), 100))
+            if (img.getPixel(x, y).approximate(BLACKPIXEL, 100))
             {
                 unsigned int minTLDist = min(getPixelDistance(x-1, y, distance_map) + 1,
                                              getPixelDistance(x, y-1, distance_map) + 1);
@@ -75,7 +83,8 @@ void getLocalMaxes (vector<vector<unsigned int>> & distance_map,
     {
         for (int x = 0; x < distance_map[y].size(); x++)
         {
-            if (distance_map[y][x] >= getPixelDistance(x+1,   y, distance_map) &&
+            if (distance_map[y][x] != 0 &&
+                distance_map[y][x] >= getPixelDistance(x+1,   y, distance_map) &&
                 distance_map[y][x] >= getPixelDistance(x+1, y-1, distance_map) &&
                 distance_map[y][x] >= getPixelDistance(  x, y-1, distance_map) &&
                 distance_map[y][x] >= getPixelDistance(x-1, y-1, distance_map) &&
@@ -87,11 +96,79 @@ void getLocalMaxes (vector<vector<unsigned int>> & distance_map,
                 local_maxes[y][x] = 1;
             }
         }
-        cout << endl;
     }
 }
 
-void printDistanceMap (vector<vector<unsigned int>> distance_map)
+void recreateImage (vector<vector<unsigned int>> & distance_map,
+                    vector<vector<unsigned int>> & local_maxes,
+                    PNG & img)
+{
+    if (distance_map.size() == 0 ||
+        distance_map.size() != local_maxes.size() ||
+        distance_map[0].size() != local_maxes[0].size())
+    {
+        cout << __FUNCTION__ << ": ERROR distance map and local maxes size mismatch" << endl;
+    }
+
+    vector<vector<vector<bool>>> visited(distance_map.size(),
+                                         vector<vector<bool>>(distance_map[0].size(),
+                                         vector<bool>(distance_map.size() + distance_map[0].size())));
+
+    for (int y = 0; y < distance_map.size(); y++)
+    {
+        for (int x = 0; x < distance_map[y].size(); x++)
+        {
+            if (local_maxes[y][x])
+            {
+                // cout << "===================processing " << x << "," << y << endl;
+                // (x, y), number of steps left
+                queue<pair<pair<unsigned int, unsigned int>, unsigned int>> pixel_queue;
+                pixel_queue.push({{x, y}, distance_map[y][x]});
+                img.setPixel(x, y, BLACKPIXEL);
+                while (!pixel_queue.empty())
+                {
+                    unsigned int currx, curry;
+                    unsigned int steps;
+                    pair<unsigned int, unsigned int> coordinates;
+                    tie(coordinates, steps) = pixel_queue.front();
+                    tie(currx, curry) = coordinates;
+                    pixel_queue.pop();
+                    if (visited[curry][currx][steps]) continue;
+                    visited[curry][currx][steps] = true;
+                    // cout << currx << "," << curry << " steps: " << steps << endl;
+
+                    if (img.getPixel(currx, curry) != BLACKPIXEL)
+                    {
+                        img.setPixel(currx, curry, Pixel(255,0,0,255));
+                    }
+
+                    if (steps != 0)
+                    {
+                        if (isPixelValid(currx+1, curry, distance_map) && !visited[curry][currx+1][steps-1])
+                        {
+                            pixel_queue.push({{currx+1, curry}, steps-1});
+                        }
+                        if (isPixelValid(currx, curry-1, distance_map) && !visited[curry-1][currx][steps-1])
+                        {
+                            pixel_queue.push({{currx, curry-1}, steps-1});
+                        }
+                        if (isPixelValid(currx-1, curry, distance_map) && !visited[curry][currx-1][steps-1])
+                        {
+                            pixel_queue.push({{currx-1, curry}, steps-1});
+                        }
+                        if (isPixelValid(currx, curry+1, distance_map) && !visited[curry+1][currx][steps-1])
+                        {
+                            pixel_queue.push({{currx, curry+1}, steps-1});
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+void printDistanceMap (vector<vector<unsigned int>> & distance_map)
 {
     for (int y = 0; y < distance_map.size(); y++)
     {
@@ -113,12 +190,14 @@ void printDistanceMap (vector<vector<unsigned int>> distance_map)
 int main () {
     cout << "Hello World!" << endl;
 
-    vector<const char *> filesin = {"../images/apple.png", "../images/batman.png", "../images/discord.png"};
-    vector<const char *> filesout = {"../out/apple.png", "../out/batman.png", "../out/discord.png"};
+    vector<const char *> filesin = {"../images/apple.png", "../images/batman.png", "../images/discord.png", "../images/cursive.png"};
+    vector<const char *> filesout = {"../out/apple.png", "../out/batman.png", "../out/discord.png", "../out/cursive.png"};
 
-    for (int i = 0; i < filesin.size(); i++) {
+    for (int i = 0; i < filesin.size(); i++)
+    {
         PNG img(filesin[i]);
-        PNG skeleton(filesin[i]);
+        PNG skeleton(img.getWidth(), img.getHeight());
+        PNG recreated(img.getWidth(), img.getHeight());
 
         vector<vector<unsigned int>> distance_map;
         getDistanceMap(distance_map, img);
@@ -126,10 +205,25 @@ int main () {
         vector<vector<unsigned int>> local_maxes;
         getLocalMaxes(distance_map, local_maxes);
 
+        for (int y = 0; y < local_maxes.size(); y++)
+        {
+            for (int x = 0; x < local_maxes[y].size(); x++)
+            {
+                if (local_maxes[y][x])
+                {
+                    skeleton.setPixel(x, y, BLACKPIXEL);
+                }
+            }
+        }
+
         printDistanceMap(distance_map);
         printDistanceMap(local_maxes);
 
-        img.write(filesout[i]);
+        recreateImage(distance_map, local_maxes, recreated);
+
+        // img.write(filesout[i]);
+        // skeleton.write(filesout[i]);
+        recreated.write(filesout[i]);
     }
 
     return 0;
